a08f709428efbaf3e35e9b252468575e867c9ab4
Nathan Drake <nathandrakedrums@gmail.com>
2026-05-20 23:21:57 -0500

    yeet

--- app/lib/feature-flags.ts
+++ app/lib/feature-flags.ts
@@ -9,7 +9,7 @@ export type FeatureFlagKey = (typeof FEATURE_FLAGS)[keyof typeof FEATURE_FLAGS];
 export const ALL_FEATURE_FLAGS: FeatureFlagKey[] = Object.values(FEATURE_FLAGS);
 
 export const FEATURE_FLAG_DEFAULTS: Record<FeatureFlagKey, boolean | string> = {
-  [FEATURE_FLAGS.PAID_FEATURES]: false,
+  [FEATURE_FLAGS.PAID_FEATURES]: true,
 };
 
 export type FeatureFlagValues = Partial<Record<FeatureFlagKey, boolean | string>>;
